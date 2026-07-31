0de9ba2673185307bc8e9404dab0c627aa2e0541
Christian <reyeschristiandave582@gmail.com>
2026-07-31 16:27:34 +0800

    Update colors

--- WINGSTOP/src/components/sections/instruction-steps.tsx
+++ WINGSTOP/src/components/sections/instruction-steps.tsx
@@ -40,7 +40,7 @@ const steps: Step[] = [
 export default function InstructionSteps() {
   return (
     <section className="relative w-full max-w-md mx-auto px-4 sm:px-5 mb-4 mt-3">
-      <div className="relative rounded-[1.5rem] sm:rounded-[2rem] p-2 sm:p-4 bg-white/5 backdrop-blur-md border-2 border-[#000001]/30 shadow-[0_8px_32_rgb(204, 0, 0)] overflow-hidden group/box transition-all duration-500 hover:border-[#000001]/50 hover:scale-[1.01]">
+      <div className="relative rounded-[1.5rem] sm:rounded-[2rem] p-2 sm:p-4 bg-white/5 backdrop-blur-md border-2 border-[#00703a]/30 shadow-[0_8px_32_rgb(204, 0, 0)] overflow-hidden group/box transition-all duration-500 hover:border-[#00703a]/50 hover:scale-[1.01]">
         <div className="absolute inset-0 rounded-[1.5rem] sm:rounded-[2rem] overflow-hidden pointer-events-none">
           <img
             src="https://i.imgur.com/i18sUbQ.jpeg"
